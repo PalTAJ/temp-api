c310a28899e51743d209be37b3dd576e6079c32c
paltaj <tajothman@std.sehir.edu.tr>
2020-02-22 23:04:18 +0300

    fixing mail service

--- src/app/services/mail_service.py
+++ src/app/services/mail_service.py
@@ -1,25 +1,45 @@
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+# import smtplib
+#
+# def send_mail(to,subject,content):
+#     msg = MIMEMultipart()
+#     message = content
+#     ###setup the parameters of the message
+#     password = "E7kFKYjVnX"
+#     msg['From'] = "metaboliticsdb@gmail.com"
+#     #
+#     # password = "KEAnun39"
+#     # msg['From'] = "tajsaleh96@gmail.com"
+#     msg['To'] = to
+#     msg['Subject'] = subject
+#
+#     msg.attach(MIMEText(message, 'plain'))
+#     server = smtplib.SMTP('smtp.gmail.com: 25')
+#     server.starttls()
+#     server.login(msg['From'], password)
+#     server.sendmail(msg['From'], msg['To'], msg.as_string())
+#     server.quit()
+#
+# # send_mail("tajothman@std.sehir.edu.tr",'test','test')
+#
+
+# from flask_mail import Message
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 def send_mail(to,subject,content):
-    msg = MIMEMultipart()
-    message = content
-    ###setup the parameters of the message
-    password = "E7kFKYjVnX"
-    msg['From'] = "metaboliticsdb@gmail.com"
-    #
-    # password = "KEAnun39"
-    # msg['From'] = "tajsaleh96@gmail.com"
-    msg['To'] = to
-    msg['Subject'] = subject
-
-    msg.attach(MIMEText(message, 'plain'))
-    server = smtplib.SMTP('smtp.gmail.com: 25')
-    server.starttls()
-    server.login(msg['From'], password)
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
-    server.quit()
-
-# send_mail("tajothman@std.sehir.edu.tr",'test','test')
-
+    message = Mail(
+        from_email='metaboliticsdb@gmail.com',
+        to_emails= to,
+        subject= subject,
+        html_content= content)
+    try:
+        sg = SendGridAPIClient('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')
+        response = sg.send(message)
+        # print(response.status_code)
+        # print(response.body)
+        # print(response.headers)
+    except Exception as e:
+        print(e)
